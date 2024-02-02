4c68e917d5f9336f91e8b599a4670ce1cea2cac0
ben <sobenkh35@gmail.com>
2024-02-02 22:14:48 +0700

    final commit

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,8 @@
 *
 * */
 
-
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -33,6 +33,8 @@ public class Main {
     static String[][] morningShift;
     static String[][] afternoonShift;
     static String[][] nightShift;
+    static String[] histories;
+    static int historyIndex;
     static String validateInput(String message,String regex){
         while (true){
             System.out.print(message);
@@ -46,6 +48,7 @@ public class Main {
     }
     public static void main(String[] args) {
         alphabet=new char[26];
+        histories=new String[100];
         int index=0;
         for(char ch='A';ch<='Z';ch++){
             alphabet[index]=ch;
@@ -56,6 +59,7 @@ public class Main {
         System.out.println("-+".repeat(50));
         hallSetUp();
         option();
+
 
     }
 
@@ -68,14 +72,16 @@ public class Main {
         nightShift=new String[row][seat];
     }
     private static void timeShow(){
+        System.out.println("-+".repeat(50));
         System.out.println("Daily show time of CSTAD Hall : ");
         System.out.println("A) Morning (10:00 AM - 12:30 PM)");
         System.out.println("B) Afternoon (03:00 PM - 05:30 PM)");
         System.out.println("C) Night (10:00 AM - 12:30 PM)");
+        System.out.println("-+".repeat(50));
     }
-
     private static void option() {
         do{
+            System.out.println("-+".repeat(50));
             System.out.println("[[ Application Menu ]]");
             System.out.println("< A >  Booking");
             System.out.println("< B >  Hall");
@@ -83,15 +89,15 @@ public class Main {
             System.out.println("< D >  Reboot");
             System.out.println("< E >  History");
             System.out.println("< F >  Exit");
-            System.out.print("Please Select Menu no :  ");
-            char op=input.next().charAt(0);
+            System.out.println("-+".repeat(50));
+            String op=validateInput("Please Select Menu no :  ","^[a-fA-F]$");
             switch (op){
-                case 'a','A' -> hallBooking();
-                case 'b','B'-> hallShow();
-                case 'c','C'-> timeShow();
-                case 'd','D'-> reboot();
-                case 'e','E'-> history();
-                case 'f','F'->{
+                case "a","A" -> hallBooking();
+                case "b","B"-> hallShow();
+                case "c","C"-> timeShow();
+                case "d","D"-> reboot();
+                case "e","E"-> history();
+                case "f","F"->{
                     System.out.println("Program has been Exit");
                     System.exit(0);
                 }
@@ -101,10 +107,22 @@ public class Main {
 
     }
     private static void history() {
+        if(!(histories[0]==null)){
+            for (String history : histories){
+                if(!(history==null)){
+                    System.out.println("-+".repeat(50));
+                    System.out.println(history);
+                    System.out.println("-+".repeat(50));
+                }
+            }
+        }else {
+            System.out.println("-+".repeat(50));
+            System.out.println("There is no history !!!!");
+            System.out.println("-+".repeat(50));
+        }
     }
 
     private static void reboot() {
-        input.nextLine();
         String confirm=validateInput("Do you want to reboot hall system ? (Y/N) : ","^(?i)[yn]$");
         if(confirm.equals("y")|| confirm.equals("Y")){
             renameArray(morningShift);
@@ -116,6 +134,7 @@ public class Main {
             morningShift=seats;
             afternoonShift=seats;
             nightShift=seats;
+            histories=new String[100];
         }else {
             System.out.println("Reboot Unsuccessfully!!!!!!!!");
         }
@@ -136,16 +155,30 @@ public class Main {
         System.out.println("-+".repeat(50));
     }
     private static void hallBooking() {
-        input.nextLine();
+        System.out.println("-+".repeat(50));
         System.out.println("Start Booking Process ");
+        System.out.println("-+".repeat(50));
         timeShow();
+        System.out.println("-+".repeat(50));
         String confirm=validateInput("Please select show time ( A | B | C ) :  ","^(?i)[abc]$");
+        System.out.println("-+".repeat(50));
         switch (confirm){
-            case "a","A" -> hallEachShift(morningShift,"Morning");
-            case "b","B" -> hallEachShift(afternoonShift,"Afternoon");
-            case "c","C" -> hallEachShift(nightShift,"Night");
+            case "a","A" -> {
+                hallEachShift(morningShift,"Morning");
+                eachShiftBooking(morningShift,"Morning");
+            }
+            case "b","B" ->{
+                hallEachShift(afternoonShift,"Afternoon");
+                eachShiftBooking(afternoonShift,"Afternoon");
+            }
+            case "c","C" -> {
+                hallEachShift(nightShift,"Night");
+                eachShiftBooking(nightShift,"Night");
+            }
         }
+        System.out.println("-+".repeat(50));
     }
+    //Rename Array To format
     private static void renameArray(String[][] seat){
         for(int i=0;i<seat.length;i++){
             for(int j=0;j<seat[i].length;j++){
@@ -168,9 +201,83 @@ public class Main {
         }
     }
 
-    private static String availableChecking(String[][] seat ,int rows,int columns){
-        if(seat[rows][columns].equals("null")||seat[rows][columns].contains("Av") ){
-            return "Available";
-        }else return "Unavailable";
+    private static void eachShiftBooking(String[][] seat ,String shift) {
+        System.out.println("-+".repeat(50));
+        System.out.println("INSTRUCTION");
+        System.out.println("Single : A-1");
+        System.out.println("Multiple (separate by comma )  : A-1,A-2");
+        System.out.println("-+".repeat(50));
+        String seatBooking = validateInput("Please select available seat :  ", "^[A-Za-z]\\s*-\\s*\\d+(?:,\\s*[A-Za-z]\\s*-\\s*\\d+)*$");
+        String[] seatCode=new String[100];
+        int index=0;
+        Scanner scanner = new Scanner(seatBooking);
+        scanner.useDelimiter(",");
+        while (scanner.hasNext()) {
+            String token = scanner.next();
+            seatCode[index]=token.toUpperCase().trim();
+            index++;
+        }
+        scanner.close();
+        boolean available=true;
+        int dex1=0,dex2=0;
+        //Available Checking
+        for (int i = 0; i < seat.length; i++){
+            for (int j = 0; j < seat[i].length; j++){
+                if(!(seatCode[dex1]==null)){
+                    if(seat[i][j].contains(seatCode[dex1].trim())){
+                        if(!(seat[i][j].contains("AV"))){
+                            available=false;
+
+                        }else{
+                            dex1++;
+                        }
+
+                    }
+                }
+            }
+        }
+        if(available)
+        {
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+            String formattedDateTime = currentDateTime.format(formatter);
+            System.out.println("-+".repeat(50));
+            int  studentID = Integer.parseInt(validateInput("Please Enter Student ID : ","^\\s*\\d+\\s*$"));
+            System.out.println("-+".repeat(50));
+            String confirm=validateInput("Are you sure to book ? (Y/N) : ","^(?i)[yn]$");
+            System.out.println("-+".repeat(50));
+            if(confirm.equals("y")|| confirm.equals("Y")){
+                String messageBlock="";
+                for (int i = 0; i < seat.length; i++) {
+                    for (int j = 0; j < seat[i].length; j++) {
+                        if(!(seatCode[dex2]==null)){
+                            if(seat[i][j].contains(seatCode[dex2].trim())){
+                                if(seat[i][j].contains("AV")){
+                                    seat[i][j]="booking";
+                                    messageBlock+="["+seatCode[dex2]+"] ";
+                                    dex2++;
+                                }
+                            }
+                        }
+                    }
+                }
+                histories[historyIndex]=("Hall "+shift+" shift: \t"+messageBlock+" Booking by Student ID :"+studentID+" \t ( "+formattedDateTime+" )");
+                historyIndex++;
+                System.out.println("-+".repeat(50));
+                System.out.println(messageBlock+" Booking Successfully");
+                System.out.println("-+".repeat(50));
+            }else {
+                System.out.println("-+".repeat(50));
+                System.out.println("Booking Unsuccessfully !!");
+                System.out.println("-+".repeat(50));
+            }
+
+        }
+        else {
+            System.out.println("-+".repeat(50));
+            System.out.println("Unavailable seat!!!!!");
+
+        }
+
     }
 }
